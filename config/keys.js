cef37a49e16a7fcd0ab3cbd83521a1ce3e7db659
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-08-09 05:36:33 +0200

    Add Rest Api Backend

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://wujot:wujot@cluster0-shard-00-00-nkrh5.mongodb.net:27017,cluster0-shard-00-01-nkrh5.mongodb.net:27017,cluster0-shard-00-02-nkrh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
+};
